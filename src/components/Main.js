0e50e737bc06009b989593d93faa9ebe7e92e97f
Matt McCormick <matt.mccormick@kitware.com>
2016-02-16 13:58:00 -0500

    BUG: Remove unused App.css.

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import React from 'react';
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
+      <div className="app">
       </div>
     );
   }
