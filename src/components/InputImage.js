75cd8d1c01a144a978da71b96fa11653a77d49bf
Matt McCormick <matt.mccormick@kitware.com>
2016-02-21 00:42:47 -0500

    ENH:  Add file input button.
    Approach suggested here:
    
      http://www.toptal.com/twitter-bootstrap/the-10-most-common-bootstrap-mistakes

--- src/components/InputImage.js
+++ src/components/InputImage.js
@@ -1,17 +1,33 @@
-//'use strict';
+'use strict';
 
 import React, { PropTypes } from 'react';
 import { Image } from 'react-bootstrap';
 
-const InputImage = ({ src, fileName, onUpload }) => (
-  <div className="panel panel-default">
-    <div className="panel-heading">Input Image</div>
-    <div className="panel-body">
-      <div className="center-block"><Image src={src} responsive /></div>
+require('styles/FileInputButton.css');
+
+
+const InputImage = React.createClass({
+  handleChange: (event) => {
+    this.props.onUpload(event.target.files[0]);
+  },
+
+  render: function() {
+    return (
+    <div className="panel panel-default">
+      <div className="panel-heading">Input Image</div>
+      <div className="panel-body">
+        <div className="center-block"><Image src={this.props.src} responsive /></div>
+      </div>
+      <div className="panel-footer">
+        {this.props.fileName}
+        <span className="btn btn-default btn-file">
+          Upload <input type="file" name="files[]" accept=".png" onChange={ this.handleChange } />
+        </span>
+      </div>
     </div>
-    <div className="panel-footer">{fileName}</div>
-  </div>
-);
+    );
+  }
+});
 
 InputImage.propTypes = {
   src: PropTypes.string.isRequired,
